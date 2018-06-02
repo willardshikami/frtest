d384c8ad8e9784bc26eb775874c4bfb3bf28e392
willardshikami <willardshikami@gmail.com>
2018-06-02 23:36:38 +0300

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewUserComponent } from './new-user/new-user.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,

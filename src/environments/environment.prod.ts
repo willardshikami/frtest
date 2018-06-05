32a5690466f925e74d8939c6fc91d38487962db8
willardshikami <willardshikami@gmail.com>
2018-06-05 08:32:03 +0300

    added deployment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDnJ12t5ODGPrcYoMtXzi-NxtfIjVFSFO8",
+    authDomain: "frtest-41292.firebaseapp.com",
+    databaseURL: "https://frtest-41292.firebaseio.com",
+    projectId: "frtest-41292",
+    storageBucket: "",
+    messagingSenderId: "544420245689"
+  }
 };
